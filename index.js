fcb8c5f60796305941e8285f4a96557457b2aa13
Omar <Omar@ISAACCRAIG-LT.originpc.local>
2021-12-02 14:30:48 -0500

    importing libraries dynamically.

--- index.js
+++ index.js
@@ -1,44 +1,8 @@
-const {
-    getCPUName,
-    getDrives,
-    getGPUName,
-    getMBName,
-    getMBRevision,
-    getMBSerial,
-    getMemSize,
-    getMemSpeed,
-    getOSName,
-    getPCName,
-    getUser,
-    getPowerGUID,
-    getPowerScheme
-} = require('./lib/get')
+var sys = {
 
-const {
-    setPCName,
-    setPCDescription,
-    setMonitorTimeout,
-    setStandbyTimeout
-} = require('./lib/set')
-
-module.exports = {
-    //get
-    getMBName,
-    getMBRevision,
-    getMBSerial,
-    getUser,
-    getCPUName,
-    getDrives,
-    getGPUName,
-    getMemSize,
-    getMemSpeed,
-    getOSName,
-    getPCName,
-    getPowerGUID,
-    getPowerScheme,
-    //set
-    setPCName,
-    setPCDescription,
-    setMonitorTimeout,
-    setStandbyTimeout,
+    get: require('./lib/get'),
+    set: require('./lib/set')
+    
 }
+
+module.exports = sys
