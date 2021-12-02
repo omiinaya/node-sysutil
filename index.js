f2e08057ceb7b3293bc5598255ee069d34285ae8
Omar <Omar@ISAACCRAIG-LT.originpc.local>
2021-12-02 14:41:01 -0500

    updating all branches.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var sys = {
 
     get: require('./lib/get'),
-    set: require('./lib/set')
-    
+    set: require('./lib/set'),
+    scripts: require('./lib/scripts')
+
 }
 
 module.exports = sys
