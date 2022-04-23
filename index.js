f76ec82264c78718f4b1790aad3e238be9538b41
omiinaya <omiinaya@gmail.com>
2022-04-22 23:24:33 -0400

    separating toggles.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var sys = {
     core: require('./lib/core'),
     get: require('./lib/get'),
     set: require('./lib/set'),
-    run: require('./lib/run')
-
+    run: require('./lib/run'),
+    toggle: require('./lib/toggle')
 }
 
 module.exports = sys
